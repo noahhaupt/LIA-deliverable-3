19cd08aa466bc9eca9ff4ebabe12092ba27ec139
noahhaupt <noahhaupt1@icloud.com>
2025-11-13 08:40:41 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+df = pd.read_csv("Global Health Statistics.csv")
+specific_countries = ["Canada", "USA"]
+data = filtered_data = df[df["Country"].isin(specific_countries)]
+#Part 3
+#Numerical variables
+numeric_variables = ["Year","Prevalence Rate (%)","Incidence Rate (%)","Mortality Rate (%)","Population Affected","Healthcare Access (%)", "Doctors per 1000","Hospital Beds per 1000", "Average Treatment Cost (USD)", "Recovery Rate (%)","DALYs","Improvement in 5 Years (%)","Per Capita Income (USD)", "Education Index", "Urbanization Rate (%)"]
+
+for x in numeric_variables:
+    print(x,"information:")
+    print(data[x].describe())
+    print("Median: ", data[x].median())
+    print("Mode: ", data[x].mode())
+    print("Variance: ", data[x].var())
+    print("Skewness: ", data[x].skew())
+    print("Kurtosis: ", data[x].kurt())
+    print()
+
+#Categorical variables
+categorical_variables = ["Country", "Disease Name", "Disease Category", "Gender", "Treatment Type", "Availability of Vaccines/Treatment", "Age Group"]
+
+for y in categorical_variables:
+    print(y,"information:")
+    print(data[y].value_counts())
+    print("Number of unique values: ", data[y].nunique())
+    print("Mode: ", data[y].mode())
+    print()
+    #Proportion???
