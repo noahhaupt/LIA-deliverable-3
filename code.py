1b5d141d9d63809ae7ccaa6de6ccd5fb9b3d1cc8
Noah Haupt <noahhaupt1@icloud.com>
2025-11-16 12:10:00 -0500

    Changed part 4

--- code.py
+++ code.py
@@ -39,133 +39,23 @@ for y in categorical_variables:
     print("Number of unique values: ", data[y].nunique())
     print("Mode: ", data[y].mode())
     print()
-    #Proportion???
     
     
 #Part 4 - Univariate graphical EDA    
 import seaborn as sns
-import matplotlib.pyplot as plt
 
-#5 variables
-numerical= ["Prevalence Rate (%)","Incidence Rate (%)","Mortality Rate (%)", "Population Affected","Healthcare Access (%)"]
+numerical = ["Prevalence Rate (%)","Incidence Rate (%)","Mortality Rate (%)","Population Affected","Healthcare Access (%)", "Doctors per 1000","Hospital Beds per 1000", "Average Treatment Cost (USD)", "Recovery Rate (%)","DALYs","Improvement in 5 Years (%)","Per Capita Income (USD)", "Education Index", "Urbanization Rate (%)"]
+#Year was removed as there is only 1 year (2024).
 
+for variable in numerical:
+    sns.displot(data, x = variable, bins=15, hue = "Country", multiple = "stack")
+    sns.displot(data, x = variable, bins = 10, hue = "Country", multiple = "dodge")
+    sns.displot(data, x = variable, bins = 10, hue = "Country", stat = "density", common_norm = False)
+    sns.displot(data, x = variable, kind = "kde", bw_adjust = .2)
+    sns.displot(data, x = variable, hue ="Country", kind = "ecdf")
+#The graphs were plotted and certain graphs were chosen for each which represent the values properly.
+# In the report, 5 numerical variables were described per team member, for a total of 15.
 
-# Histograms
-for col in numerical:
-    sns.histplot(data[col], bins=15)
-    plt.show()
-
-# By Gender
-for col in numerical:
-    sns.displot(data, x=col, hue="Gender", kind="hist", bins=15)
-    plt.show()
-
-# Stacked histogram
-for col in numerical:
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="stack")
-    plt.show()
-
-# Dodge bars
-for col in numerical:
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="dodge")
-    plt.show()
-
-# Normalized histograms
-for col in numerical:
-    sns.histplot(data[col], bins=15, stat="probability")
-    plt.show()
-
-# KDE
-for col in numerical:
-    sns.displot(data, x=col, hue="Gender", kind="kde")
-    plt.show()
-
-# Emperical cumulative dist
-for col in numerical:
-    sns.displot(data, x=col, kind="ecdf" )
-    plt.show()
-    
-
-#5 new variables
-numerical = ["Doctors per 1000","Hospital Beds per 1000","Average Treatment Cost (USD)", "Recovery Rate (%)","Per Capita Income (USD)"]    
-
-
-#Histograms
-for col in numerical: 
-    sns.histplot(data[col], bins=15)
-    plt.show()
-
-#By gender
-for col in numerical: 
-    sns.displot(data, x=col, hue="Gender", kind="hist", bins=15) 
-    plt.show()
-    
-#Stacked histograms    
-for col in numerical: 
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="stack") 
-    plt.show()
-    
-#Dodge bars
-for col in numerical: 
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="dodge")
-    plt.show()
-    
-#Normalized histograms
-for col in numerical:
-    sns.histplot(data[col], bins=15, stat="probability") 
-    plt.show()
-    
-#KDE
-for col in numerical: 
-    sns.displot(data, x=col, kind="kde") 
-    plt.show()
-    
-#Emperical cumulative dist
-for col in numerical: 
-    sns.displot(data, x=col, kind="ecdf")
-    plt.show()
-    
-    
-#5 new variables
-numerical= ["Education Index","Urbanization Rate (%)","Improvement in 5 Years (%)", "DALYs","Year"]
-
-
-#Histograms
-for col in numerical: 
-    sns.histplot(data[col], bins=15) 
-    plt.show()
-    
-#By gender
-for col in numerical:
-    sns.displot(data, x=col, hue="Gender", kind="hist", bins=15) 
-    plt.show()
-    
-#Stacked histogram   
-for col in numerical: 
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="stack") 
-    plt.show()
-
-#Dodge bars
-for col in numerical:
-    sns.histplot(data, x=col, hue="Gender", bins=15, multiple="dodge") 
-    plt.show()
-    
-#Normalized histograms
-for col in numerical:
-    sns.histplot(data[col], bins=15, stat="probability") 
-    plt.show()
-    
-#KDE
-for col in numerical: 
-    sns.displot(data, x=col, kind="kde") 
-    plt.show()
-    
-#Emperical cumulative dist
-for col in numerical: 
-    sns.displot(data, x=col, kind="ecdf") 
-    plt.show()
-    
-    
     
 #Part 5 - Multivariate Non-Graphical EDA
 #a)
@@ -199,21 +89,17 @@ import seaborn as sns
 
 #Question A: How does Prevalance Rate (%) relate to Mortality Rate (%) in Canada vs USA?
 sns.relplot(data=data, x="Prevalence Rate (%)", y="Mortality Rate (%)", col="Country")
-plt.show()
 
 #Question B: How do Treatment Type, Population Affected and Country relate to Average Treatment Cost and Recovery Rate?
 sns.relplot(data=data, x="Average Treatment Cost (USD)", y="Recovery Rate (%)", hue="Treatment Type", size="Population Affected", col="Country")
-plt.show()
 
 #Question C: How has Mortality Rate (%) changed over the years in Canada vs USA?
 sns.relplot(data=data, x="Year", y="Mortality Rate (%)", kind="line", hue="Country")
-plt.show()
 
 #Question D:
     
 #Question E: In Canada, is there a linear relationship between Healthcare Access (%) and Recovery Rate (%)?
 sns.lmplot(data=data, x="Healthcare Access (%)", y="Recovery Rate (%)")
-plt.show()
 
 
 
@@ -223,13 +109,10 @@ plt.show()
 
 # Question A: How do Prevalence Rate (%) and Treatment cost vary together?
 sns.displot(data=data, x="Prevalence Rate (%)", y="Average Treatment Cost (USD)", kind="hist", binwidth=(5, 500), cbar=True)
-plt.show()
 
 # Question B: What is the smooth density pattern between Healthcare Access (%) and Recovery Rate (%)? 
 sns.displot(data=data, x="Healthcare Access (%)", y="Recovery Rate (%)", kind="kde", levels=10, thresh=0.05)
-plt.show()
 
 # Question C: How does the joint distribution of Doctors per 1000 vs Hospital Beds per 1000 differ between Canada and USA?
 sns.displot(data=data, x="Doctors per 1000", y="Hospital Beds per 1000", hue="Country", kind="kde")
-plt.show()
 
